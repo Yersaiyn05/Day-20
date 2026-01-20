ae3634ec274def7994a10a9a4c2174bab7f19eb7
Yersaiyn <yersaiynkulbayev@gmail.com>
2026-01-20 12:50:21 +0500

    HomeWork22

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,7 +1,31 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body, Put, Patch } from '@nestjs/common';
 import { TasksService } from './tasks.service';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { UpdateTaskDto } from './dto/UpdateTask.Dto';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private readonly tasksService: TasksService) {}
+
+  @Get('all')
+  findAll() {
+    return this.tasksService.findAll();
+  }
+
+  @Get(':id')
+  findById(@Param('id') id: string) {
+    return this.tasksService.findById(Number(id));
+  }
+
+  @Post('create')
+  create(@Body() dto: CreateTaskDto) {
+    console.log('DTO:',dto);
+    return this.tasksService.createTask(dto);
+  }
+
+
+  @Patch(':id')
+  update(@Param('id') id:string, @Body() dto:UpdateTaskDto){
+    return this.tasksService.update(Number(id),dto);
+  }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,49 @@
-import { Injectable } from '@nestjs/common';
-
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { UpdateTaskDto } from './dto/UpdateTask.Dto';
 @Injectable()
-export class TasksService {}
+export class TasksService {
+    private tasks = [
+        {
+            id: 1,
+            title: 'Task 1',
+            description: 'Description 1',
+        },
+        {
+            id: 2,
+            title: '',
+            description: '',
+        },
+    ];
+
+    findAll() {
+        return this.tasks;
+    }
+    findById(id: number) {
+        const task = this.tasks.find((task) => task.id === id);
+        if (!task) {
+            throw new NotFoundException('Task not found');
+        }
+        return task;
+    }
+
+    createTask(dto: CreateTaskDto) {
+        const newTask = {
+            id: this.tasks.length + 1,
+            ...dto
+        };
+        this.tasks.push(newTask);
+        return newTask;
+    }
+
+    update(id:number,dto:UpdateTaskDto){
+        const task = this.findById(id);
+        if(dto.title !== undefined){
+            task.title = dto.title;
+        }
+        if(dto.description !== undefined){
+            task.description = dto.description;
+        }
+        return task;
+    }
+}
